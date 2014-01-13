047348108f071aebef8a0a36252ff4fcb54acac8
Tom Geudens <tomgeu@zeus.(none)>
2014-01-13 11:58:02 +0100

    sparql endpoint locked down

--- urn.com.elbeesee.kbodata.server/resources/groovy/kbodatasparql.groovy
+++ urn.com.elbeesee.kbodata.server/resources/groovy/kbodatasparql.groovy
@@ -13,6 +13,13 @@ import org.netkernel.layer0.nkf.*;
 import org.netkernel.layer0.representation.*
 import org.netkernel.layer0.representation.impl.*;
 
+
+/**
+ * Processing Imports.
+ */
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * KBO data sparql Accessor.
@@ -25,17 +32,44 @@ INKFRequestContext aContext = (INKFRequestContext)context;
 //
 
 String vQuery = aContext.source("httpRequest:/param/query", String.class);
+String vAcceptHeader = aContext.source("httpRequest:/header/Accept", String.class);
 
 // processing
 if (vQuery != null) {
 	// protect against injection
+	Pattern vInjectionPattern = Pattern.compile("(?i)\\bINSERT|DELETE|LOAD|CLEAR|CREATE|DROP|COPY|MOVE|ADD\\b");
+	Matcher vInjectionMatcher = vInjectionPattern.matcher(vQuery);
+	Boolean vInjectionFound = vInjectionMatcher.find();
+	
+	if (vInjectionFound) {
+		vQuery = null;
+	}
 }
+
+if (vAcceptHeader == null) {
+	// provide sensible default Accept
+	Pattern vConstructPattern = Pattern.compile("(?i)\\bCONSTRUCT\\b");
+	Matcher vConstructMatcher = vConstructPattern.matcher(vQuery);
+	Boolean vConstructFound = vConstructMatcher.find();
+	
+	if (vConstructFound) {
+		vAcceptHeader = "application/rdf+xml";
+	}
+	else {
+		vAcceptHeader = "application/sparql-results+xml";
+	}
+}
+
 INKFRequest sparqlrequest = aContext.createRequest("active:kbodatastardogsparql");
-sparqlrequest.addArgumentByValue("query", vQuery);
+if (vQuery != null) {
+	sparqlrequest.addArgumentByValue("query", vQuery);
+}
+sparqlrequest.addArgumentByValue("accept", vAcceptHeader);
 Object vResult = aContext.issueRequest(sparqlrequest);
 //
 
 
 INKFResponse vResponse = aContext.createResponseFrom(vResult);
+vResponse.setMimeType(vAcceptHeader);
 vResponse.setExpiry(INKFResponse.EXPIRY_DEPENDENT);
 //
